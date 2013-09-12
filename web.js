20281721003ffecc8244dd226a66195357f1d183
Sam Hoffman <sam.m.hoffman@gmail.com>
2013-09-12 02:35:01 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!!');
+ var message = buf.toString(fs.readFileSync(index.html))
+ response.send(message);
 });
 
 var port = process.env.PORT || 5000;
